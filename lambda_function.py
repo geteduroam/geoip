3559ee164938ba1dd151618c59bfef1d39e1d1dc
Jørn Åne de Jong <git@jornane.no>
2020-06-16 14:00:03 +0200

    Add support for CloudFront

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,28 @@
 from geoip import geoip_json
 
 def lambda_handler(event, context):
+	# event['requestContext']['http']['sourceIp']
+	# event['requestContext']['identity']['sourceIp']
+	# event['headers']['X-Forwarded-For']
+	e = event
+	e = e.get('headers', e)
+	e = e.get('requestContext', e)
+	e = e.get('http', e)
+	e = e.get('identity', e)
+	ip = e.get('sourceIp', None) or e.get('X-Forwarded-For', '').split(',')[0]
+
 	return {
 		'statusCode': 200,
 		'headers': {
 			'Content-Type': 'application/json',
 			'Cache-Control': 'no-store',
 		},
-		'body': geoip_json(event['requestContext']['http']['sourceIp']),
+		'body': geoip_json(ip),
+	} if ip else {
+		'statusCode': 400,
+		'headers': {
+			'Content-Type': 'text/plain',
+			'Cache-Control': 'no-store',
+		},
+		'body': 'Unable to find client IP address in event object',
 	}
